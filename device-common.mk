e32e7c3de719cb86eec58e264a3b8c715a0c6b21
TARKZiM <tom8476oo@gmail.com>
2021-04-18 17:43:10 +0800

    kitakami-common: Remove LiveDisplay HAL
    * Kernel drivers causing some panic on other device,
      so just remove it for now.

--- device-common.mk
+++ device-common.mk
@@ -223,10 +223,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.msm8994
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sysfs
-
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \

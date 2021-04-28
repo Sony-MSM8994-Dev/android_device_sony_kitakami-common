62ddceb1c18113032d01fec64bc1d8c55d26d548
TARKZiM <tom8476oo@gmail.com>
2021-04-29 00:13:31 +0800

    Revert "kitakami-common: Remove LiveDisplay HAL"
    This reverts commit e32e7c3de719cb86eec58e264a3b8c715a0c6b21.

--- device-common.mk
+++ device-common.mk
@@ -236,6 +236,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.msm8994
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \

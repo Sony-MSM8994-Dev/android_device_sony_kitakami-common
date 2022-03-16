9183554c0d7d646b6faf123b5b982f71936509f7
TARKZiM <tom8476oo@gmail.com>
2022-03-16 19:57:37 +0800

    kitakami-common: Binderize camera provider and override service
    * We need binderize service to load HAL on S.
    * HAL service must be in mutiple OEM groups to access secd.

--- device-common.mk
+++ device-common.mk
@@ -125,7 +125,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-impl:32 \
     camera.msm8994 \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
